5e6cf3eb0d71234bf82d4555b956496d73a550fa
Marcelo <marcelorocha.dev0@gmail.com>
2026-08-25 15:37:41 -0300

    atualizando configuração do Firebase e ajustando URL do Google Apps Script

--- js/config.js
+++ js/config.js
@@ -1,19 +1,43 @@
 // ============================================
 // Configuração - TalentoUNICAP
 // ============================================
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
+import {
+  getAuth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+} from "https://www.gstatic.com/firebasejs/12.18.0/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCxiOfxy6HPAiJ4hz3fmYPamNl8XpVb-IE",
+  authDomain: "talentos-unicap.firebaseapp.com",
+  projectId: "talentos-unicap",
+  storageBucket: "talentos-unicap.firebasestorage.app",
+  messagingSenderId: "936743315009",
+  appId: "1:936743315009:web:3d4964704577c1934548c0",
+  measurementId: "G-0BLD35NF7J"
+};
+
+// Inicializa o Firebase
+const app = initializeApp(firebaseConfig);
+
+// Inicializa a autenticação
+const auth = getAuth(app);
+
+export {
+  app,
+  auth,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut
+};
+
 const CONFIG = {
   // URL do Google Apps Script (depois de publicar como app web)
-  API_URL: 'https://script.google.com/macros/s/SUA_URL_AQUI/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxIfpDwJifTVvh7jvGCzLVo13-8veciil9aBzuzIKd0xrzvLe7-4WhIpppIXN9iChLl/exec',
   
-  // Configuração do Firebase
-  firebase: {
-    apiKey: "SUA_API_KEY_AQUI",
-    authDomain: "seu-projeto.firebaseapp.com",
-    projectId: "seu-projeto",
-    storageBucket: "seu-projeto.appspot.com",
-    messagingSenderId: "123456789",
-    appId: "1:123456789:web:abcdef"
-  },
+
   
   // Estados brasileiros
   states: [
